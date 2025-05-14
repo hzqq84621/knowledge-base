6faf1cb8c431195cf9156a60d21a3ed6b92a1780
Yubin-Fang <fangyubin@vip.163.com>
2025-05-13 19:28:08 -0700

    fix: 优化canvas_list接口，黄老师修改了对话权限；更新用户设置页面，移除部门和群组选项

--- web/src/pages/user-setting/setting-team/index.tsx
+++ web/src/pages/user-setting/setting-team/index.tsx
@@ -5,15 +5,13 @@ import {
 import { Button, Card, Flex, Space, Tabs } from 'antd';
 import { useTranslation } from 'react-i18next';
 
-import { TeamOutlined, UserAddOutlined, UserOutlined, PartitionOutlined, PlusOutlined } from '@ant-design/icons';
+import { TeamOutlined, UserAddOutlined } from '@ant-design/icons';
+import { useState } from 'react';
 import AddingUserModal from './add-user-modal';
 import { useAddUser } from './hooks';
 import styles from './index.less';
 import TenantTable from './tenant-table';
 import UserTable from './user-table';
-import DepartmentTable from './department-table';
-import GroupTable from './group-table';
-import { useState } from 'react';
 
 const iconStyle = { fontSize: 20, color: '#1677ff' };
 
@@ -39,20 +37,6 @@ const UserSettingTeam = () => {
         </>
       ),
     },
-    {
-      key: 'departments',
-      label: '部门',
-      children: (
-        <>
-          <DepartmentTable />
-        </>
-      ),
-    },
-    {
-      key: 'groups',
-      label: '群组',
-      children: <GroupTable />,
-    },
   ];
 
   return (
@@ -69,11 +53,7 @@ const UserSettingTeam = () => {
         </Flex>
       </Card>
       <Card bordered={false}>
-        <Tabs 
-          activeKey={activeTab}
-          onChange={setActiveTab}
-          items={tabItems}
-        />
+        <Tabs activeKey={activeTab} onChange={setActiveTab} items={tabItems} />
       </Card>
       <Card
         title={
